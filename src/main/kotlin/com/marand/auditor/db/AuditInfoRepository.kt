63172d63d91f257a9fdca250ac948f92111137ef
Nejc Korasa <nejc.korasa@marand.si>
2017-10-02 07:54:58 +0200

    use Jpa repository

--- src/main/kotlin/com/marand/auditor/db/AuditInfoRepository.kt
+++ src/main/kotlin/com/marand/auditor/db/AuditInfoRepository.kt
@@ -1,10 +1,10 @@
 package com.marand.auditor.db
 
 import com.marand.auditor.db.model.AuditInfoEntity
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
 /**
  * @author Nejc Korasa
  */
 
-interface AuditInfoRepository : CrudRepository<AuditInfoEntity, Long>
+interface AuditInfoRepository : JpaRepository<AuditInfoEntity, Long>
