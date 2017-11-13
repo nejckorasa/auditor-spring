9ba7a1d66ca1c516151e1a403cf85f03064fb59f
Nejc Korasa <nejc.korasa@marand.si>
2017-11-13 15:40:32 +0100

    introduce separate logger for failed audits

--- src/main/kotlin/com/marand/auditor/AuditRecovery.kt
+++ src/main/kotlin/com/marand/auditor/AuditRecovery.kt
@@ -4,32 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.marand.auditor.dto.AuditInfo
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardOpenOption
 import java.util.*
 
 /**
  * @author Nejc Korasa
  */
 
-private const val FAILED_AUDITS_FILE = "failed-audits.txt"
-
 @Component
 open class AuditRecovery(private var objectMapper: ObjectMapper) {
 
     private val logger = LoggerFactory.getLogger(AuditRecovery::class.java)
+    private val failedLogger = LoggerFactory.getLogger("failed-log")
 
     open fun logFailed(auditInfo: AuditInfo, ex: Exception?) {
 
-        Files.newBufferedWriter(Paths.get(FAILED_AUDITS_FILE), StandardOpenOption.APPEND, StandardOpenOption.CREATE)
-                .use({
-                    it.append(objectMapper.writeValueAsString(auditInfo))
-                    it.newLine()
-                })
+        val json = objectMapper.writeValueAsString(auditInfo)
 
-        logger.error("SAVING FAILED [object, error] :" +
-                "${objectMapper.writeValueAsString(auditInfo)}, ${ex?.let { Arrays.toString(it.stackTrace) }}")
+        failedLogger.info(json)
+        logger.error("SAVING FAILED [object, error] :" + "$json, ${ex?.let { Arrays.toString(it.stackTrace) }}")
     }
-
 }
